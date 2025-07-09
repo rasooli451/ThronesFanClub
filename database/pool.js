070228d69dcc5f24bd5fd74bff970487423a46f4
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-07-09 01:46:38 -0400

    the pool database configuration

---
+++ database/pool.js
@@ -0,0 +1,14 @@
+
+
+
+require("dotenv").config();
+const {Pool} = require("pg");
+
+
+module.exports = new Pool({
+    host : process.env.HOST,
+    user : process.env.USER,
+    database : process.env.DATABASE,
+    password : process.env.PASSWORD,
+    port : 5432
+});
