6da801cb2623fd09a9a844aa28036097c092affa
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-07-09 01:48:21 -0400

    the router to handle all homepage routes

---
+++ routes/HomePageRouter.js
@@ -0,0 +1,29 @@
+
+
+
+const {Router} = require("express");
+
+
+
+const HomePageRouter = Router();
+
+
+
+
+
+
+HomePageRouter.get("/", (req, res)=>{
+
+})
+
+
+HomePageRouter.get("/profile", (req,res)=>{
+
+})
+
+
+
+
+
+
+module.exports = HomePageRouter;
