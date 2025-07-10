c59c0ce23052b734338a7b75839482b4e175324e
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-07-10 01:20:09 -0400

    added routes for creating new posts

--- routes/HomePageRouter.js
+++ routes/HomePageRouter.js
@@ -6,15 +6,21 @@ const {Router} = require("express");
 
 
 const HomePageRouter = Router();
+const HomePageIndexController = require("../controllers/homepage/HomePageIndexController");
+const HomePageNewPostController = require("../controllers/homepage/HomePageNewPostController");
 
 
 
 
+HomePageRouter.get("/", HomePageIndexController);
 
 
-HomePageRouter.get("/", (req, res)=>{
-
+HomePageRouter.get("/post", (req, res)=>{
+    res.render("newpost.ejs");
 })
+
+HomePageRouter.post("/post", HomePageNewPostController);
+
 
 
 HomePageRouter.get("/profile", (req,res)=>{
