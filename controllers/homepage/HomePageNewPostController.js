7235ac114b59063f6ba292deb5d3e74279676a40
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-07-12 01:40:20 -0400

    added validation for submitted data

--- controllers/homepage/HomePageNewPostController.js
+++ controllers/homepage/HomePageNewPostController.js
@@ -6,7 +6,7 @@ const {body,validationResult} = require("express-validator");
 
 
 const validateMessage = [
-    body("message").escape().trim().isLength({min : 1, max : 3000}).withMessage("The post should be between 1 and 3000 characters")
+    body("message").trim().isLength({min : 1, max : 3000}).withMessage("The post should be between 1 and 3000 characters")
 ];
 
 
