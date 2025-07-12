0f04f5d2286b22796ebf5fd42fe9ce8f885bd3ec
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-07-12 01:41:10 -0400

    controller for handling post requests for editing comments

---
+++ controllers/profile/CommentEditPostController.js
@@ -0,0 +1,24 @@
+
+
+
+const {editComment} = require("../../database/queries");
+const {body, validationResult} = require("express-validator");
+
+const validateEdit = [
+    body("comment").trim().isLength({min : 1, max : 500}).withMessage("Comments should be between 1 and 500 characters")
+];
+
+const CommentEditPostController = [validateEdit, async function(req, res){
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(500).render("errors", {errors : errors.array()})
+    }
+    const {comment, message_id} = req.body;
+    const {commentid} = req.params;
+    await editComment(commentid, comment);
+    res.redirect(`/profile/comment/${message_id}`);
+}]
+
+
+
+module.exports = CommentEditPostController;
