1d6f00d35fe8be4097da878978086d215091a9b9
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-07-14 19:45:32 -0400

    took into account the case when admin deletes the user when admin is logged in, deletes the user and gets redirected to homepage instead of getting logged out

--- controllers/profile/DeleteUserController.js
+++ controllers/profile/DeleteUserController.js
@@ -5,18 +5,26 @@ const {deleteUser} = require("../../database/queries");
 
 
 const DeleteUserController = async function(req, res){
-    const {userid} = req.params;
-    req.logout(err => {
-    if (err) return res.status(500).render("errors", {errors : [err]});
-
-    req.session.destroy(async err => {
+    const {userinfo} = req.params;
+    const userid = userinfo.split(",")[0];
+    const fromUser = userinfo.split(",")[1];
+    if (fromUser == "true"){
+        await deleteUser(userid);
+        res.redirect("/homepage");
+    }
+    else{
+         req.logout(err => {
         if (err) return res.status(500).render("errors", {errors : [err]});
 
-        await deleteUser(userid);
-        res.clearCookie('connect.sid');
-        res.redirect('/login');
-    });
-    });
+        req.session.destroy(async err => {
+            if (err) return res.status(500).render("errors", {errors : [err]});
+
+            await deleteUser(userid);
+            res.clearCookie('connect.sid');
+            res.redirect('/login');
+        });
+        });
+    }
 }
 
 
