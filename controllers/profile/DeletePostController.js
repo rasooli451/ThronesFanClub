896c0f3f22476ecdf33735d8a36533da8e348f6a
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-07-14 19:44:42 -0400

    fixed the redirection, now it redirects correctly based on from where it was requested

--- controllers/profile/DeletePostController.js
+++ controllers/profile/DeletePostController.js
@@ -1,11 +1,18 @@
-const {deletePost} = require("../../database/queries");
+const {deletePost,getUserIdFromMessageId} = require("../../database/queries");
 
 
 
 const DeletePostController = async function(req, res){
-    const {postid} = req.params;
+    const {postinfo} = req.params;
+    const postid = postinfo.split(",")[0];
+    const fromUser = postinfo.split(",")[1];
+    let redirectURL = "/profile";
+    if (fromUser == "true"){
+        const owner = await getUserIdFromMessageId(postid);
+        redirectURL = `/profile/user/${owner}`;
+    }
     await deletePost(postid);
-    res.redirect("/profile");
+    res.redirect(redirectURL);
 }
 
 
