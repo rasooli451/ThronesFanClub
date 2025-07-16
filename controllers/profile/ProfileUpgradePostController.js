9a9581ed8c333ab16c5271cefa08b8bc5eb8326f
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-07-15 23:26:37 -0400

    the controller for displaying the riddle question

---
+++ controllers/profile/ProfileUpgradePostController.js
@@ -0,0 +1,19 @@
+
+const {updateMembership} = require("../../database/queries");
+require("dotenv").config();
+
+
+const ProfileUpgradePostController = async function(req, res){
+    const loggedInUserId = req.user.user_id;
+    const {answer} = req.body;
+    if (answer.trim().toUpperCase() == process.env.ANSWER){
+        await updateMembership(loggedInUserId);
+        res.redirect("/homepage");
+    }
+    else{
+        res.render("errors", {errors : [{msg : "Incorrect, Please Try again!"}]});
+    }
+}
+
+
+module.exports = ProfileUpgradePostController;
