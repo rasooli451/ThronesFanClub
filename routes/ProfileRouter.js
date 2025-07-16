606df9c2b36a305efe9b2fe89491286f00f18dac
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-07-15 23:41:16 -0400

    added routes for updating membership

--- routes/ProfileRouter.js
+++ routes/ProfileRouter.js
@@ -17,6 +17,7 @@ const EditCommentController = require("../controllers/profile/EditCommentControl
 const CommentEditPostController = require("../controllers/profile/CommentEditPostController");
 const DeleteCommentController = require("../controllers/profile/DeleteCommentController");
 const UserProfileIndexController = require("../controllers/profile/UserProfileIndexController");
+const ProfileUpgradePostController = require("../controllers/profile/ProfileUpgradePostController");
 
 
 ProfileRouter.get("/", ProfileIndexController);
@@ -45,6 +46,18 @@ ProfileRouter.post("/comment/edit/:commentid", CommentEditPostController);
 ProfileRouter.get("/comment/delete/:commentinfo", DeleteCommentController);
 
 ProfileRouter.get("/user/:id", UserProfileIndexController);
+
+ProfileRouter.get("/upgrade", (req, res)=>{
+    if (req.user == undefined){
+      return res.render("errors", {errors : [{msg : "You are not logged in, please Log In first."}]})
+    }
+    if (req.user.ismember){
+        return res.render("errors", {errors : [{msg : "You are already a member."}]})
+    }
+    res.render("upgrade");
+})
+
+ProfileRouter.post("/upgrade", ProfileUpgradePostController);
 
 module.exports = ProfileRouter;
 
