89be57dc66766f2f8979f865905bc9769608d221
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-07-22 00:12:18 -0400

    increased max # of characters for comment from 500 to 1000

--- database/populatedb.js
+++ database/populatedb.js
@@ -18,7 +18,7 @@ const SQL = `
      favorite VARCHAR (100)
      );
 
-     INSERT INTO users (username,password,email,isAdmin,isMember) VALUES ('Farhad Rasoli', '$2b$10$nuTe3MJ69x8CpOu2aFQK/uML/KF7/pw8y7RciyGTZ9nvfcdrbFHVi', 'rasoolyfarhad7@gmail.com', 1, 1);
+     INSERT INTO users (username,password,email,isAdmin,isMember) VALUES ('The Bastard King', '$2b$10$nuTe3MJ69x8CpOu2aFQK/uML/KF7/pw8y7RciyGTZ9nvfcdrbFHVi', 'rasoolyfarhad7@gmail.com', 1, 1);
 
 
      CREATE TABLE IF NOT EXISTS messages(
@@ -32,7 +32,7 @@ const SQL = `
 
      CREATE TABLE IF NOT EXISTS comments(
      comment_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-     comment VARCHAR (500),
+     comment VARCHAR (1000),
      owner_id INTEGER,
      message_id INTEGER);
 
